9f3f10708ca87739d3abe260a4e7454ccad6c663
MattiaRigon <rigonmattia@gmail.com>
2023-02-06 17:04:40 +0100

    change text

--- gui/gui.py
+++ gui/gui.py
@@ -6,7 +6,8 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-
+from PyQt5.QtWidgets import * 
+from PyQt5.QtGui import * 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import requests
 
@@ -29,9 +30,10 @@ class Ui_MainWindow(object):
         MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(60, 30, 671, 41))
-        self.textEdit.setObjectName("textEdit")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(60, 30, 671, 41))
+        self.label.setFont(QFont('Arial', 25))
+        self.label.setObjectName("label")
         self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
         self.scrollArea.setGeometry(QtCore.QRect(60, 90, 671, 351))
         self.scrollArea.setWidgetResizable(True)
@@ -73,11 +75,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.textEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:18pt;\">LIST OF DOCKERS</span></p></body></html>"))
+        self.label.setText(_translate("MainWindow","LIST OF DOCKERS"))
         i=0
         for item in elenco_docker :
             self.radioButton[i].setText(_translate("MainWindow","docker"+ item))
@@ -106,7 +104,7 @@ class Ui_MainWindow(object):
         print("Richiesta TURN ON docker : " + docker_selected)
         data["action"] = "TurnOn"
         data["DockerID"] = docker_selected.replace("docker","")
-        
+
         r = requests.post(SERVER_IP,json=data)
 
 
@@ -125,4 +123,3 @@ class Ui_MainWindow(object):
     
     
         
-  
