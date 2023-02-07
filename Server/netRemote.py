34189c7a0477339c75fae25a1398bc36da393861
MattiaRigon <rigonmattia@gmail.com>
2023-02-07 17:01:17 +0100

    add file

--- Server/netRemote.py
+++ Server/netRemote.py
@@ -14,15 +14,13 @@ def myNetwork():
 
     info( '*** Adding controller\n' )
     net.addController(name='c0', controller=RemoteController)
-    #net.addController(name='c0')
     info( '*** Add switches\n')
     s1 = net.addSwitch('s1')
     Intf( 'eth1', node=s1 )
 
     info( '*** Add hosts\n')
     h1 = net.addHost('h1', ip='0.0.0.0', mac='aa:bb:cc:dd:ee:01') #DHCP 
-    #h2 = net.addHost('h2', ip='0.0.0.0', mac='aa:bb:cc:dd:ee:02')
-    #h1 = net.addHost('h1', ip='10.230.161.110') #IP STATICO CIAL
+    
     info( '*** Add links\n')
     net.addLink(h1, s1)
     #net.addLink(h2, s1)
@@ -30,7 +28,6 @@ def myNetwork():
     info( '*** Starting network\n')
     net.start()
     h1.cmdPrint('dhclient '+h1.defaultIntf().name) #DHCP
-    #h2.cmdPrint('dhclient '+h2.defaultIntf().name)
     
     h1.cmd('sudo python3 hostServer.py &')
 
