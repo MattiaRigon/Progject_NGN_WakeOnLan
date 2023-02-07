161ebdf9648cd627519bd40436581f7089e2778b
MattiaRigon <rigonmattia@gmail.com>
2023-02-07 15:03:43 +0100

    update hostserver.py

--- Server/hostServer.py
+++ Server/hostServer.py
@@ -7,10 +7,11 @@ from wakeonlan import send_magic_packet
 import requests
        
 MAX_CONTAINERS = 3
-IPRPI1 = "192.168.43.176"
-IPRPI2 = ""
-MACRPI1 = "14.18.c3.58.9f.e4"
-MACRPI2 = ""
+IPRPI1 = "192.168.43.188"          #PIETRO: 192.168.43.188   #CASA LUCA: 192.168.1.180
+MACRPI1 = "b8:27:eb:2d:cb:df"
+
+IPRPI2 = "192.168.43.147"           #PIETRO: 192.168.43.147  #CASA LUCA: 192.168.1.80
+MACRPI2 = "b8:27:eb:0d:0a:72"
 
 
 
@@ -26,18 +27,18 @@ class MyServer(BaseHTTPRequestHandler):
         R1off = False
         R2off = False
         self.send_response(200)
-        text = self.rfile.read(int(self.headers['Content-Length']))
-        data = json.loads(text.decode('utf-8'))
+        textByte = self.rfile.read(int(self.headers['Content-Length']))
+        dataJson = json.loads(textByte.decode('utf-8'))
         self.end_headers()
+
 
         try:
             r = requests.get(f'http://{IPRPI1}:8000')    #RASPBERRY 1
             print("RASP1")
-
+            
             n1 = int(r.text)
-            if n1<MAX_CONTAINERS:
-                r = requests.post(f'http://{IPRPI1}:8000', json=data)
-
+            if n1<MAX_CONTAINERS:  #se c'è posto               
+                r = requests.post(f'http://{IPRPI1}:8000', data=textByte)
                 self.wfile.write("Eseguo il docker sul Raspy 1".format(self.path).encode('utf-8'))
                 return
             else:
@@ -51,8 +52,8 @@ class MyServer(BaseHTTPRequestHandler):
             r = requests.get(f'http://{IPRPI2}:8000')    #RASPBERRY 2
             n2 = int(r.text)
             if n2<MAX_CONTAINERS:
-                r = requests.post(f'http://{IPRPI2}:8000', data=text)
-                
+                r = requests.post(f'http://{IPRPI2}:8000', data=textByte)
+
                 self.wfile.write("Eseguo il docker sul Raspy 2".format(self.path).encode('utf-8'))
                 return
 
@@ -64,25 +65,20 @@ class MyServer(BaseHTTPRequestHandler):
         if R1off:    #RPI1 OFF 
             send_magic_packet(MACRPI1)
             sleep(5)
-            r = requests.post(f'http://{IPRASP1}:8000', data=text)
-            
+            r = requests.post(f'http://{IPRPI1}:8000', data=textByte)
+
             self.wfile.write("Eseguo il docker sul Raspy 1".format(self.path).encode('utf-8'))
         elif R2off:    #RPI2 OFF
             send_magic_packet(MACRPI2)
             sleep(5)
-            r = requests.post(f'http://{IPRPI2}:8000', data=text)
-            
+            r = requests.post(f'http://{IPRPI2}:8000', data=textByte)
+
             self.wfile.write("Eseguo il docker sul Raspy 2".format(self.path).encode('utf-8'))
         else:           #BOTH FULL
             print("SEGMENTATION FAULT")
 
             self.wfile.write("Segmentation Fault".format(self.path).encode('utf-8'))
 
-    
-
-        # os.path.dirname(os.path.abspath(__file__))
-        # esecuzione = os.popen('./wakeon.sh {0}'.format(text.decode('utf-8'))).read()
-        # print(esecuzione)
         return
 
 if __name__ == '__main__':
