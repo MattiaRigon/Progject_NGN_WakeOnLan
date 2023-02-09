e11a96e1bc1cffda808590d73914ad2ba3457dd8
MattiaRigon <70909197+MattiaRigon@users.noreply.github.com>
2023-02-09 18:24:10 +0100

    change port and comments

--- Raspberry/sniffer.py
+++ Raspberry/sniffer.py
@@ -2,9 +2,9 @@ from scapy.all import *
 import os
 
 def packet_callback(packet):
-    if packet[UDP].dport == 9:
+    if packet[UDP].dport == 9:  # one raspberry port 9 , the other one port 10
         print("Magic packet detected!")
-        os.system("lxterminal -e 'bash -c \"sudo python3 /home/pi/Desktop/next2022/server.py\"'")
+        os.system("lxterminal -e 'sudo python3 /home/pi/Desktop/next2022/server.py'")
             #lanci il server.py
 
 print("Sniffing...")    
