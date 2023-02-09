77ccde2fe86b6322a921d0b42bd3d3a48372ffdd
MattiaRigon <70909197+MattiaRigon@users.noreply.github.com>
2023-02-09 18:24:42 +0100

    add comments

--- Server/netRemote.py
+++ Server/netRemote.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
+# netRemote.py - python script that creates the Mininet virtual network
+# One host h1 used for the web server that routes the requests traffic, the virtual switch s1 is linked with the interface eth1 to allow the 
+# communication with the real network
+
 from mininet.net import Mininet
 from mininet.node import Controller, RemoteController, OVSController
 from mininet.cli import CLI
 from mininet.link import Intf
 from mininet.log import setLogLevel, info
+import subprocess
 
 
 def myNetwork():
@@ -14,16 +19,17 @@ def myNetwork():
 
     info( '*** Adding controller\n' )
     net.addController(name='c0', controller=RemoteController)
+
     info( '*** Add switches\n')
     s1 = net.addSwitch('s1')
-    Intf( 'eth1', node=s1 )
+    Intf( 'eth1', node=s1 )   #Add interface eth1 to switch s1 to allow connection with real network
 
     info( '*** Add hosts\n')
     h1 = net.addHost('h1', ip='0.0.0.0', mac='aa:bb:cc:dd:ee:01') #DHCP 
-    
+    #h1 = net.addHost('h1', ip='192.168.43.119', mac='aa:bb:cc:dd:ee:01')   #STATIC
+
     info( '*** Add links\n')
     net.addLink(h1, s1)
-    #net.addLink(h2, s1)
 
     info( '*** Starting network\n')
     net.start()
